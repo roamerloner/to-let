d644969878174aff916a78af4bf2e27393124493
Asif Khan <khanarnob806@gmail.com>
2023-07-27 16:11:59 +0600

    Initial commit

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,15 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import {getFirestore} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB-iopdai54HXHpbcvONI_0xvSHFc5euQI",
-  authDomain: "to-let-f9821.firebaseapp.com",
-  projectId: "to-let-f9821",
-  storageBucket: "to-let-f9821.appspot.com",
-  messagingSenderId: "355942244186",
-  appId: "1:355942244186:web:24501820efea1d2654dd63"
+  apiKey: "AIzaSyBwmDvm3jpzXyNbu7FwfGz0ybOTJAJRZLo",
+  authDomain: "to-let-80c58.firebaseapp.com",
+  projectId: "to-let-80c58",
+  storageBucket: "to-let-80c58.appspot.com",
+  messagingSenderId: "476583125025",
+  appId: "1:476583125025:web:c013ad38ddfc7e6a0a0360"
 };
 
 // Initialize Firebase
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link, useNavigate} from 'react-router-dom'
 import Layout from '../components/Layout/Layout'
 
 const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+     email: '',
+     name: '',
+     password: ''
+  });
+  const {name, email, password} = formData;
+  const navigate = useNavigate();
+  const onChange = () => {} 
   return (
     <Layout>
-        <h1>SignUp Page</h1>
+        <div className='d-flex align-items-center justify-content-center w-100 mt-4'>
+            <form>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputEmail1" className="form-label">Enter Name</label>
+                  <input type="text" value={name} className="form-control" id="name" onChange={onChange} aria-describedby="nameHelp" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                  <input type="email" value={email} onChange={onChange} className="form-control" id="email" aria-describedby="emailHelp" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                  <input type="password" value={password} onChange={onChange} className="form-control" id="password" />
+                </div>
+                <button type="submit" className="btn btn-primary">Sign up</button>
+                <div>
+                  <h6>Login with Google</h6>
+                  <span>Already User</span> <Link to="/signin">Login</Link>
+                </div>
+            </form>
+
+        </div>
     </Layout>
-  )
-}
+  );
+};
 
 export default SignUp
