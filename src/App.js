8535497908959b8711af7d9c748ade4999a94da5
HIMU <mirazkamalhimel10@gmail.com>
2023-08-14 10:33:41 +0600

    updated App.js for new routes

--- src/App.js
+++ src/App.js
@@ -7,17 +7,22 @@ import Offers from "./pages/Offers";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <BrowserRouter>
     <ToastContainer />
     <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/offers" element={<Offers/>}/>
-      <Route path="/signin" element={<SignIn/>}/>
-      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/offers" element={<Offers/>} />
+      <Route path="/signin" element={<SignIn/>} />
+      <Route path="/profile" element={<PrivateRoute/>}>
+        <Route path="/profile" element={<Profile/>} />
+      </Route>
       <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/forgot-password" element={<ForgotPassword/>}/>
     </Routes>
     </BrowserRouter>
   );
